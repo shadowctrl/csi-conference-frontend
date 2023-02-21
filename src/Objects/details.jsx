274eab426231ddcb4caaaf466e7510d7c6a2bc17
ThePythonGuyy <thepythonguy@outlook.com>
2023-02-21 20:06:18 +0530

    Added chiefPatrons objects

---
+++ src/Objects/details.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+
+
+export const chiefPatrons = [
+
+    {
+        'name':    'Dr. Ishari K. Ganesh',
+        'about':   'founder, chairman and Chancellor of Vels University, Chennai',
+        'img':     '/images/chiefPatrons/ishariGanesh.jpg'
+    },
+
+    {
+        'name':    'Prof. Dr. S. Sadagopan',
+        'about':   'Chairman, IIITDM, Kancheepuram',
+        'img':     '/images/chiefPatrons/sadagopan.jpg'
+    },
+    
+    {
+        'name':    'Dr. B. Chidhambararajan',
+        'about':   'Director-SRMVEC & Past and Founder Chairman-CSI Kancheepuram Chapter',
+        'img':     '/images/chiefPatrons/chidhambaram.jpg'
+    },
+
+    {
+        'name':    'Mr. H. R. Mohan',
+        'about':   `Mr. H. R. Mohan, Fellow & Former President, CSI Past Chair, IEEE CS Madras & ACM Chennai, 
+                    Former AVP (Systems), The Hindu`,
+        'img':     '/images/chiefPatrons/hrMohan.jpg'
+    },
+
+    // {
+    //     'name': '',
+    //     'about': '',
+    //     'img': ''
+    // 
+]
