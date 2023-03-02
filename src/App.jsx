a2fdf673989f0a1ee7028f25677bb368c6465079
shadowctrl <shadowctrl@outlook.com>
2023-03-02 11:42:30 +0530

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,63 @@
 import "./App.css";
 /*              COMPONENTS              */
-// import Header from "./components/Header";
+import Header from "./components/Header";
 import Hero from "./components/hero";
-import ChiefGuest from "./components/chiefGuest"
+// import ChiefGuest from "./components/chiefGuest";
 // import chief from "./components/chief";
-// import Info from "./components/Info";
+import Info from "./components/Info";
 // import Aboutus from "./components/aboutus";
-// import chiefGuest from "./components/chiefGuest";
+import ChiefGuest from "./components/chiefGuest";
 
 /*               LAYOUTS               */
 import RootLayout from "./Layouts/RootLayout";
 
-
-import { createRoutesFromElements, createBrowserRouter, Route, RouterProvider, NavLink } from "react-router-dom";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
 // import Popups from './components/Popups';
 // const router = cre
-const router = createBrowserRouter (
-               createRoutesFromElements(
+// const router = createBrowserRouter (
+//                createRoutesFromElements(
 
-                <Route path='/' element={<RootLayout/>}>
+//                 <Route path='/' element={<RootLayout/>}>
 
-                    <Route index element={<Hero/>} />
-                    <Route path='chiefGuest' element={<ChiefGuest/>} />
-                </Route>
-              
+//                     <Route index element={<Hero/>} />
+//                     <Route path='chiefGuest' element={<ChiefGuest/>} />
+//                 </Route>
 
-               )
-              )
+//                )
+//               )
 
 function App() {
   return (
-    <div className="">
-      {/* <div>
-        {" "}
-        <Header />
-      </div>
-      <div>
-        {" "}
-        <Hero />
-      </div>
-    
-      <div>
-        {" "}
-        <Info />{" "}
-      </div> */}
-      <RouterProvider router={router}/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={[<Header />, <Hero />, <Info />]} />
+        <Route
+          path="chiefGuest"
+          element={[<Header />, <ChiefGuest />, <Info />]}
+        ></Route>
+      </Routes>
+    </BrowserRouter>
+    // <div className="">
+    //   <div>
+    //     {" "}
+    //     <Header />
+    //   </div>
+    //   <div>
+    //     {" "}
+    //     <Hero />
+    //   </div>
+
+    //   <div>
+    //     {" "}
+    //     <Info />{" "}
+    //   </div>
+    //   {/* <RouterProvider router={router}/> */}
+    // </div>
   );
 }
 
