4284e5c65b749508c8de369df64c6ab2783b8378
Shadowctrl <42708326+shadowctrl@users.noreply.github.com>
2023-03-21 13:39:09 +0530

    Delete src directory

--- src/components/sponsors.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import error from "./error";
-const sponsors = () => {
-  return <Error />;
-};
-
-export default sponsors;
--- src/components/ud.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ud = () => {
-  return <div>Site Under Development</div>;
-};
-
-export default ud;
